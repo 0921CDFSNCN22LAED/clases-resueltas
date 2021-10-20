ec5d2abbc6ea79a97dbc5f94d6575bdbf695b423
Pablo Baleztena <pablo@ultharsoftware.com>
2021-10-19 21:55:06 -0300

    clase 12 completa

--- 12-html/app.js
+++ 12-html/app.js
@@ -4,9 +4,10 @@ const path = require("path");
 
 app.listen(3000);
 
+//  / === "public" ///   /css/styles.css
 app.use(express.static(path.join(__dirname, "public")));
 
 //localhost:3000/
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/home.html"));
+    res.sendFile(path.join(__dirname, "views/home.html"));
 });
