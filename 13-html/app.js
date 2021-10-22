4a71a5192bd8ba1f4c0296ef00b2c10e166625e5
Pablo Baleztena <pablo@ultharsoftware.com>
2021-10-22 07:36:44 -0300

    clase 13

---
+++ 13-html/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(3000);
+
+//  / === "public" ///   /css/styles.css
+app.use(express.static(path.join(__dirname, "public")));
+
+//localhost:3000/
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/home.html"));
+});
