37e86d09eea48208d8d60d864907677822d30945
Pablo Baleztena <pablo@ultharsoftware.com>
2021-11-03 13:21:55 -0300

    hasta clase 16

--- 16-html/app.js
+++ 16-html/app.js
@@ -15,3 +15,7 @@ app.get("/", (req, res) => {
 app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, "views/register.html"));
 });
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"));
+});
